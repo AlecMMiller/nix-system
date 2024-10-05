9297da2fe8077fb65e57d74c3cbcf3c0b2bd83a6
Alec Miller <alecmichaelmiller@gmail.com>
2024-10-04 17:34:26 -0700

    cleaner per device setup

--- hosts/kami/configuration.nix
+++ hosts/kami/configuration.nix
@@ -1,15 +1,14 @@
-{ config, pkgs, inputs, ... }:
-let
-sources = import ./npins;
-in
 {
-  imports =
-    [ # Include the results of the hardware scan.
+  inputs,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
     ../../manifest.nix
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-#(sources.catppuccin + "/modules/home-manager")
-    ];
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
 
   virt = {
     enable = true;
@@ -20,74 +19,7 @@ in
     nvidia = true;
   };
 
-  services.logind.extraConfig = ''
-    HandlePowerKey=ignore
-    '';
-
-  programs.thunar.enable = true;
-  programs.steam.enable = true;
-  networking.hostName = "kami"; # Define your hostname.
-# Set your time zone.
-    time.timeZone = "America/Phoenix";
-
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-
-# Configure keymap in X11
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-    videoDrivers = ["nvidia"];
-  };
-
-  services.displayManager.sddm = {
-    enable = true;
-    theme = "catppuccin-mocha";
-    package = pkgs.kdePackages.sddm;
-  };
-  services.blueman.enable = true;
-
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-  environment.systemPackages = with pkgs; [
-#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#  wget
-    home-manager
-      xorg.xeyes
-      qemu
-      lxqt.lxqt-policykit
-      (catppuccin-sddm.override {
-       flavor = "mocha";
-       font = "Noto Sans";
-       fontSize = "9";
-       loginBackground = false;
-       })
-  ];
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  programs.fish.enable = true;
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWAARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-  };
-
-  system.autoUpgrade = {
-    enable = true;
-    dates = "*-*-* 04:00:00";
-    flake = "github:AlecMMiller/nix-system";
-  };
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
--- manifest.nix
+++ manifest.nix
@@ -1,7 +1,10 @@
-{ ... }:
-
 {
-  programs.dconf.enable = true;
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
 
   imports = [
     ./i18n.nix
@@ -14,4 +17,78 @@
     ./hardware.nix
     ./networking.nix
   ];
+
+  config = {
+    programs.dconf.enable = true;
+    programs.thunar.enable = true;
+    programs.steam.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+
+    services.logind.extraConfig = ''
+      HandlePowerKey=ignore
+    '';
+
+    # Set your time zone.
+    time.timeZone = "America/Phoenix";
+
+    services.displayManager.sddm = {
+      enable = true;
+      theme = "catppuccin-mocha";
+      package = pkgs.kdePackages.sddm;
+    };
+    services.blueman.enable = true;
+
+    environment.systemPackages =
+      with pkgs;
+      [
+        home-manager
+        xorg.xeyes
+        qemu
+        sbctl
+        unzip
+        brightnessctl
+        lxqt.lxqt-policykit
+        (catppuccin-sddm.override {
+          flavor = "mocha";
+          font = "Noto Sans";
+          fontSize = "9";
+          loginBackground = false;
+        })
+      ]
+      ++ lib.optionals config.virt.enable [
+        qemu
+      ];
+
+    programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    programs.fish.enable = true;
+
+    environment.sessionVariables = {
+      WLR_NO_HARDWAARE_CURSORS = "1";
+      NIXOS_OZONE_WL = "1";
+    };
+
+    system.autoUpgrade = {
+      enable = true;
+      dates = "*-*-* 04:00:00";
+      flake = "github:AlecMMiller/nix-system";
+    };
+
+    services.xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+
+  };
 }
