a6091c42ccf1cd2215e3a1125ae54c046fed8472
Alec Miller <alecmichaelmiller@gmail.com>
2024-10-07 07:26:08 -0700

    sync blender configs

--- syncthing.nix
+++ syncthing.nix
@@ -11,12 +11,17 @@ with lib;
         devices = {
           "kumo" = {
             id = "LLGDXIY-JRJ4EEX-LSIVPM7-UWFBEMZ-H6XQKD7-PUMVJAE-X7TBIJM-FJSADAR";
+            address = "tcp://192.168.1.4:22000";
           };
           "icarus" = mkIf (config.host.name != "icarus") {
             id = "77KXY3B-722AGF2-7TIQMSM-PIL4IIE-SM2JTEN-F6CZVZZ-IX2KPEQ-EDKVJAO";
           };
         };
         folders = {
+          BlenderConfig = {
+            path = "/home/alec/.config/blender";
+            devices = [ "kumo" ] ++ optionals (config.host.name != "icarus") [ "icarus" ];
+          };
           Documents = {
             path = "/home/alec/Documents";
             devices = [ "kumo" ] ++ optionals (config.host.name != "icarus") [ "icarus" ];
