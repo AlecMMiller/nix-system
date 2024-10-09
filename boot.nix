8e311a866921e1bbe0ced7a04cd8b700ad601fe2
Alec Miller <alecmichaelmiller@gmail.com>
2024-10-09 11:37:46 -0700

    use kernel 6.10 with nvidia

--- boot.nix
+++ boot.nix
@@ -8,41 +8,47 @@ with lib;
 {
   options.lanzaboote = {
     enable = mkOption {
-    type = types.bool;
-    default = false;
+      type = types.bool;
+      default = false;
     };
   };
   config = {
     boot = mkMerge [
-    (if config.lanzaboote.enable then {
-      loader.systemd-boot.enabke = lib.mkForce false;
-      lanazaboote = {
-        enable = true;
-        pkiBundle = "etc/secureboot";
-      };
-    } else {
-      loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
-      };
-    })
-    {
-      bootspec.enable = true;
+      (
+        if config.lanzaboote.enable then
+          {
+            loader.systemd-boot.enabke = lib.mkForce false;
+            lanazaboote = {
+              enable = true;
+              pkiBundle = "etc/secureboot";
+            };
+          }
+        else
+          {
+            loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+          }
+      )
+      {
+        bootspec.enable = true;
 
+        kernelParams =
+          lists.optionals config.graphics.intel [
+            "nvme.noacpi=1"
+            "mem_sleep_default=deep"
+            "i915.force_probe=7dd5"
+          ]
+          ++ lists.optionals config.graphics.nvidia [
+            "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+          ];
 
-      kernelParams =
-        lists.optionals config.graphics.intel [
-          "nvme.noacpi=1"
-          "mem_sleep_default=deep"
-          "i915.force_probe=7dd5"
-        ]
-        ++ lists.optionals config.graphics.nvidia [
-          "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-        ];
+        kernelPackages =
+          if config.graphics.nvidia then pkgs.linuxKernel.packages.linux_6_10 else pkgs.linuxPackages_latest;
 
-      kernelPackages = pkgs.linuxPackages_latest;
-
-      supportedFilesystems = [ "ntfs" ];
-    }];
+        supportedFilesystems = [ "ntfs" ];
+      }
+    ];
   };
 }
