bd79160ca1531791399cd901f7db2c8beff67e50
Alec Miller <alecmichaelmiller@gmail.com>
2024-12-13 06:57:51 -0700

    new kumo in syncthing

--- apps/syncthing.nix
+++ apps/syncthing.nix
@@ -1,5 +1,4 @@
-{ config, lib, ... }:
-with lib;
+{ ... }:
 {
   config = {
     services.syncthing = {
@@ -9,7 +8,7 @@ with lib;
       configDir = "/home/alec/.config/syncthing";
       settings = {
         devices = {
-          "kumo" = {
+          "kumo_old" = {
             id = "DK5JUOS-3BEZ4QW-7F7I4SJ-62U7SGU-CLNK6ZR-ODI4CPH-INOBS2S-DSW5UQ2";
             addresses = [ "tcp://192.168.1.4:22000" ];
           };
@@ -19,6 +18,10 @@ with lib;
           "kami" = {
             id = "ITCI6FM-LZDEGUS-YI5SYS2-YT6NIYJ-EFHMQNB-PCOZVHB-ZE3G7RT-KZ3JWQZ";
             addresses = [ "tcp://192.168.1.167:22000" ];
+          };
+          "kumo" = {
+            id = "J7SF67L-HXDAI27-BCX3OAM-XC2NUNK-BB4TNZX-RCRNYTK-SMR25HO-DHQMXAO";
+            addresses = [ "tcp://192.168.1.17:22000" ];
           };
         };
         folders = {
